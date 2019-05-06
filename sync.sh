e00da4e40bce41b3a8cc75f0efe70827b3d038b0
Dan Seremet <seremet.dan@gmail.com>
2019-05-06 13:59:23 +0100

    Added onedrive configs and fixed sync.sh script to actually work. It's like tested and everything

--- sync.sh
+++ sync.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-HOME=~
+HOME=/home/dans
+SCR=$HOME/Documents/scripts
+cd $HOME
+
 
 # replaces the files in HOME with (links to) the ones in this folder
 for NAME in ".profile" ".bash_aliases" ".bashrc" ".vimrc"; do
-    rm $HOME/$NAME
-    ln -s $NAME $HOME/$NAME
+    rm -f $NAME
+    echo "Linking $SCR/$NAME to $(pwd)/$NAME"
+    ln -s $SCR/$NAME $NAME
 done
+
+
+# sync the onedrive configs
+OD_DIR=$HOME/.config/onedrive
+cd $OD_DIR
+
+mkdir -p uni
+mkdir -p personal
+
+for NAME in "starter" "uni/config" "personal/config"; do
+    rm -f $NAME
+    echo "Linking $SCR/$NAME to $(pwd)/$NAME"
+    ln -s $SCR/onedrive/$NAME ./$NAME
+done
+
