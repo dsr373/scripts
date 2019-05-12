d76d3a9f7168b526c381480ce52b2a51731e688d
Dan Seremet <seremet.dan@gmail.com>
2019-05-12 21:06:53 +0100

    Script to install stuff after fresh system re-install

---
+++ install.sh
@@ -0,0 +1,16 @@
+cd ~
+
+sudo apt install python3 python3-pip golang
+sudo apt install zathura pqiv ranger redshift speedcrunch
+sudo apt install vim neovim
+
+snap install code --classic
+snap install skype --classic
+snap install spotify
+snap install mailspring
+
+# chrome
+cd ~/Downloads
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+cd ~
