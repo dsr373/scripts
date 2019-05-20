47412b20fa125f9bcd3c57ae89f228fccf22d877
Dan Seremet <seremet.dan@gmail.com>
2019-05-20 12:04:47 +0100

    Added python packages and some essetial stuff

--- install.sh
+++ install.sh
@@ -1,13 +1,21 @@
 cd ~
 
+# absolutely necessary
+sudo apt install git
+# sysadmin
+sudo apt install tlp --install-recommends
+# tools
 sudo apt install python3 python3-pip golang
-sudo apt install zathura pqiv ranger redshift speedcrunch onedrive
 sudo apt install vim neovim
+sudo apt install zathura zathura-djvu pqiv ranger redshift speedcrunch onedrive vlc
+# input
+sudo apt install xdotool libinput-tools ruby
+sudo gem install fusuma
 
-snap install code --classic
-snap install skype --classic
-snap install spotify
-snap install mailspring
+sudo snap install code --classic
+sudo snap install skype --classic
+sudo snap install spotify
+sudo snap install mailspring
 
 # set up go env
 source .profile
@@ -22,3 +30,6 @@ cd ~/Downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
 cd ~
+
+# python libs
+sudo pip3 install matplotlib numpy scipy
